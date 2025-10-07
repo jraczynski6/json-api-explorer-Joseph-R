f6b4a1f98283017ca3f5e2aeef5a5f4751d26580
jraczynski <raczynski.joseph616@gmail.com$ git config --global user.email raczynski.joseph616@gmail.com>
2025-10-06 23:17:57 -0500

    WIP: Add code for group project

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// so much empty
+
+const postList = document.getElementById("postList");
+const fetchButton = document.getElementById("fetchButton");
+const error = document.getElementById("error");
+const myForm = document.getElementById("postForm");
+
+fetchButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    console.log(event.target)
+   if (event.target.innerText === "Fetch Posts") {
+        //console.log("WE DID IT")
+        fetch("https://jsonplaceholder.typicode.com/posts").then(response => {
+            response.json().then(data => {
+                console.log(data);
+                // postList.innerHTML = `<p>${data[0].title}</p>`
+                for (const post of data) {
+                    // debugger
+                    postList.innerHTML += `<p>${post.title}</p>`
+                }
+            })
+            
+        })
+    }
+})
